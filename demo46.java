b572f3e4e889cb5d75a4e1e0b4e23b8a3875fbe6
KirtiSrivastava1811 <kirtisrivastava.004@gmail.com>
2024-10-03 09:51:01 +0530

    add new paragraph

---
+++ demo46.java
@@ -0,0 +1,16 @@
+ class A 
+ {
+    public A()
+    {
+        System.out.println("object created");
+    }
+    {
+        System.out.println("in A show");
+    }
+}
+        public class demo46 {
+    public static void main(String[] args) {
+      new A();
+}
+        }
+
